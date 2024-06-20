fd9a7e42292cb9c7157f3697b8cbea0a06aa2590
FrazierMark <m.mark.frazier@gmail.com>
2024-06-20 11:04:15 -0700

    Broadcast message when someone leaves are joins. Also we keep track of the number of people in the chat.

--- index.js
+++ index.js
@@ -5,30 +5,40 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
 
+let userCount = 0;
+
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+	console.log('a user connected');
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
+	userCount++;
+	io.emit('user joined', {
+		count: userCount,
+		id: socket.id,
+	});
 
-  socket.on('typing', () => {
-    socket.broadcast.emit('typing', socket.id);
-  });
+	socket.on('chat message', (msg) => {
+		io.emit('chat message', msg);
+	});
 
-  socket.on('stop typing', () => {
-    socket.broadcast.emit('stop typing', socket.id);
-  });
+	socket.on('typing', () => {
+		socket.broadcast.emit('typing', socket.id);
+	});
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
+	socket.on('stop typing', () => {
+		socket.broadcast.emit('stop typing', socket.id);
+	});
+
+	socket.on('disconnect', () => {
+		console.log('user disconnected');
+
+		userCount--;
+		io.emit('user left', userCount);
+	});
 });
-
 
 server.listen(3000, () => {
 	console.log('listening on *:3000');
